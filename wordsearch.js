67e5df264ac7fbb21963755cd64585d68ee473e0
Yuze Wang <yuzewang621@gmail.com>
2023-05-07 15:15:10 -0600

    fulfill wordsearch in array

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  let find = false;
+  //Check horizontal first
+  if (checkWord(horizontalJoin,word)) {
+    return true;
+  }
+  const verticalArray = getVerticalArray(letters);
+  const verticalJoin = verticalArray.map(ls=> ls.join(''));
+  find = checkWord(verticalJoin,word);
+  return find;
+};
 
-module.exports = wordSearch
+const checkWord = (array, word) =>{
+  for (let l of array) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+const getVerticalArray = (horizontalJoin)=> {
+ 
+  let result = [];
+  for (let i = 0; i < horizontalJoin[0].length; i++) {
+    let vertical = [];
+    for (let j = 0; j < horizontalJoin.length; j++) {
+      vertical.push(horizontalJoin[j][i]);
+    }
+    result.push(vertical);
+  }
+
+  return result;
+};
+
+
+module.exports = wordSearch;
